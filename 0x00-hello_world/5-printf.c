b87a932459d40a7e43e3f1f244f61510f39591ef
Derrick Atongo <derrickatongo@gmail.com>
2023-02-23 01:58:22 -0800

    Print an output using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - this is the entry point
+ *
+ * Return: always gives 0
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,/n");
+	return (0);
+}
