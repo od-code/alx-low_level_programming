0d6e1f45d2d85dd060470811cd7f26be21e4ee61
Derrick Atongo <derrickatongo@gmail.com>
2023-02-25 00:39:51 -0800

    print all numbers with commas and a space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry pont
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
