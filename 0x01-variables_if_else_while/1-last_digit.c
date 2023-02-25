142447c48ca1dc555626d7d93f98259981d9a62c
Derrick Atongo <derrickatongo@gmail.com>
2023-02-24 22:32:43 -0800

    fixed the checker

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	else if (o == 0)
 		printf("Last digit of %d is %d and is 0\n", n, o);
 	else if (o < 6 && o != 0)
-		printf("Last digit of %d is %d and is less than 6 abd not 0\n", n, o);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, o);
 
 
 	return (0);
