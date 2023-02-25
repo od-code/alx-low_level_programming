d53805efc1d9a8dbe1bd614a9d0a83c14547e8a1
Derrick Atongo <derrickatongo@gmail.com>
2023-02-24 22:38:33 -0800

    removed a space in the output

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	o = n % 10;
 
 	if (o > 5)
-		printf("Last digit of %d is %d  and is greater than 5\n", n, o);
+		printf("Last digit of %d is %d and is greater than 5\n", n, o);
 	else if (o == 0)
 		printf("Last digit of %d is %d and is 0\n", n, o);
 	else if (o < 6 && o != 0)
