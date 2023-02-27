e176b6e5a2a5f4f0317d28a317e082f3456a0fb5
Derrick Atongo <derrickatongo@gmail.com>
2023-02-27 03:07:51 -0800

    Prints last digit of a value

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of number
+ *
+ * @i: the argument to be checked
+ *
+ * Return: Last digit of a number
+ */
+
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -k;
+	return (k);
+}
