77ba2223123f7b4e63ff01e34e645a50cf5df2c5
Derrick Atongo <derrickatongo@gmail.com>
2023-02-27 00:49:37 -0800

    Check if number is great than equal to or less than o

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if a number is greater than, equal to, less than 0
+ *
+ *@n: takes an integer as an argument
+ *
+ * Return: Always 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
