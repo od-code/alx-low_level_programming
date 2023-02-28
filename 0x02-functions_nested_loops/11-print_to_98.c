122b0a428068392d33dc4c64a16aca856023b6fb
Derrick Atongo <derrickatongo@gmail.com>
2023-02-28 01:18:12 -0800

    print natural numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function to print natural numbers
+ *
+ * @a: function parameter
+ */
+
+void print_to_98(int a)
+{
+	int b;
+
+	if (a >= 0 && a <= 98)
+	{
+		for (b = a; b <= 98; b++)
+		{
+			printf("%d, ", b);
+		}
+		printf("\n");
+	}
+	else if (a < 0)
+	{
+		for (b = a; b <= 98; b++)
+		{
+			printf("%d, ", b);
+		}
+		printf("\n");
+	}
+	else if (a > 98)
+	{
+		for (b = a; b >= 98; b++)
+		{
+			printf("%d, ", b);
+		}
+		printf("\n");
+	}
+	else if (a == 98)
+		printf("%d, ", a);
+}
