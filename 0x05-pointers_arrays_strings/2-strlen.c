9be57fc54aa8afbb600c68d05bc884bedc61391a
Derrick Atongo <derrickatongo@gmail.com>
2023-02-28 17:40:35 -0800

    Prints string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: function parameter
+ *
+ * Return: Anlways 0
+ */
+
+int _strlen(char *s)
+{
+	int n = 0;
+
+	for (; *s++;)
+		n++;
+	return (a);
+}
