1cb8801c9434ea647650a2f68901e8f2c29274af
Derrick Atongo <derrickatongo@gmail.com>
2023-03-01 05:30:29 -0800

    Task 08

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_array - Prints n elementsf an array of integers
+ *
+ * @a: Array of integers
+ *
+ * @n: Number of elements of the array to be printed
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		printf("%d", a[j]);
+		if (j != (n - 1))
+		{
+			printtf(", ");
+		}
+	}
+	printf("\n");
+}
