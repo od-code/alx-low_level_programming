984252240b49c2c132d7c7c5b1472c1b7e41a05f
Derrick Atongo <derrickatongo@gmail.com>
2023-03-05 03:49:20 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper - function checks if the input is uppercase or lowercase
+ *
+ * @c: function parameter to be checked
+ *
+ * Return: returns 1 if uppercase and 0  if lowercase
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
