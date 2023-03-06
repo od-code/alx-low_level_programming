66ecc1a2daf7b37f2524d7acab3d35efa7e57c38
Derrick Atongo <derrickatongo@gmail.com>
2023-03-06 11:33:25 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,10 +12,13 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			if (j < 0)
+			{
+			_putchar((j / 10) + '0');
+			}
+		_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
 
