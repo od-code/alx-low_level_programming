29e46d7eb2310d105164e722a7e6b05bc832c798
Derrick Atongo <derrickatongo@gmail.com>
2023-03-13 06:51:19 -0700

    Task 1: Copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -18,10 +18,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	int i = 0;
 	int j = n;
 
-	for  (; j < i; j++)
+	for  (; i < j; i++)
 	{
-	dest[j] = src[j];
-	n--;
+		dest[i] = src[i];
+		n--;
 	}
 	return (dest);
 }
