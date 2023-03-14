3b7fbea335bcf416d5d8515fbd4852e0a3119a02
Derrick Atongo <derrickatongo@gmail.com>
2023-03-14 05:56:01 -0700

    Task 5: Returns natural squareroot of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,7 +5,6 @@ int actual_sqrt_recursion(int n, int i);
 /**
  * _sqrt_recursion - finds the natural squareroot of a number
  *
- *
  * @n: the number
  *
  * Return: the squareroot
@@ -17,3 +16,23 @@ int _sqrt_recursion(int n)
 		return (-1);
 	return (actual_sqrt_recursion(n, 0));
 }
+
+ /**
+  * actual_sqrt_recursion - finds natural square root
+  *
+  * @n: the number
+  *
+  * @i: iterator
+  *
+  * Return: the squareroot
+  */
+
+int actual_sqrt_recursion(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (actual_sqrt_recursion(n, i + 1));
+}
+
