04dd251f80ba818625938e52eb689c76f4803bb9
Derrick Atongo <derrickatongo@gmail.com>
2023-03-14 21:29:36 -0700

    First commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc _attribute_((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
