b0e86c1ffc28b18de32522a107b038e90d832392
Derrick Atongo <derrickatongo@gmail.com>
2023-03-16 17:19:06 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -46,18 +46,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
 	char *ptr;
-	
+
 	if (nmemb == 0 || size == 0)
 
 		return (NULL);
-	
+
 	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
 		return (NULL);
-	
+
 	_memset(ptr, 0, nmemb * size);
-	
+
 	return (ptr);
 
 }
