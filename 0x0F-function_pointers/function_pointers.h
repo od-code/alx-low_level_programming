ef40772eeeaa6ce672341c47e54db91f6ced5a98
Derrick Atongo <derrickatongo@gmail.com>
2023-03-26 01:43:53 -0700

    Added header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+#include <stdlib.h>
+
 int_putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
@@ -10,4 +13,4 @@ int int_index(int *array, int size, int (*cmp)(int));
 
 
 
-#endif 
+#endif
