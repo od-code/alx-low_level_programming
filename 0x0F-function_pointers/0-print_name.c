65f2f460a819024e2e7c2af90557f66825d0ee9d
Derrick Atongo <derrickatongo@gmail.com>
2023-03-26 01:48:42 -0700

    Added spaces to the comments

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,9 +2,12 @@
 #include <stdio.h>
 /**
  * print_name - print name using pointer to function
+ *
  * @name: string to add
+ *
  * @f: pointer to function
- * Return: nothing
+ *
+ * Return: void
  **/
 void print_name(char *name, void (*f)(char *))
 {
