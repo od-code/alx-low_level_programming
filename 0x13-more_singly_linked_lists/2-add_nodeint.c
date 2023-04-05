d567702f6865fdba7f9f1c218d2c5eeffec3315c
Derrick Atongo <derrickatongo@gmail.com>
2023-04-05 11:21:28 -0700

    fixed the checker

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -16,8 +16,10 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
+
 	new->n = n;
 	new->next -= *head;
 	*head = new;
+
 	return (new);
 }
