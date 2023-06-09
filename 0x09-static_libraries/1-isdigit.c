39e12aece217fad01b8280e61c7f2c735ea57383
Derrick Atongo <derrickatongo@gmail.com>
2023-06-09 17:17:29 +0300

    Added static libraries and variadic function tasks

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit from 0 thorugh 9
+ *
+ * @c: input value
+ *
+ * Return: 1 if c is a digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
